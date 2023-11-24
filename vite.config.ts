0ca99152c6765fc986817702e327b5529b5e0c07
david dong <hi@daviddong.me>
2023-11-24 11:45:22 +0800

    feat: use javascript singlefile bundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
